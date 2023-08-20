76843879aec201ea256950d74c0ec129b2df9fdb
Kateryna <ikatya7575@gmaiil.com>
2023-08-20 15:22:29 +0300

    finished

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,41 @@
+const loaderP = document.querySelector(".loader")
+const selectCats = document.querySelector(".breed-select");
+const catInfoDiv = document.querySelector(".cat-info");
+const BREEDS_VOID = `https://api.thecatapi.com/v1/images/`;
+import Notiflix from 'notiflix';
 
+ 
 function fetchBreeds(url) {
-    return fetch(`${url}`).then(
+ return fetch(`${url}`).then(
         (resp) => {
             if (!resp.ok) {
-                throw new Error(resp.statusText)
+                loaderP.classList.add("hidden")
+               Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+            throw new Error(resp.statusText)
             }
+            loaderP.classList.add("hidden")
+            selectCats.classList.remove("hidden")
             return resp.json()
         }
     )
 }
-export {fetchBreeds}
+
+function fetchCatByBreed(id) {
+    catInfoDiv.classList.add("hidden")
+    loaderP.classList.remove("hidden")
+     return fetch(`${BREEDS_VOID}${id}`).then(
+        (resp) => {
+             if (!resp.ok) {
+                 loaderP.classList.add("hidden")
+               Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+                throw new Error(resp.statusText)
+             }
+             loaderP.classList.add("hidden")
+               catInfoDiv.classList.remove("hidden")
+            return resp.json()
+        }
+    )
+}
+export {fetchBreeds,fetchCatByBreed, BREEDS_VOID, loaderP,selectCats, catInfoDiv}
 
    
--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
 import axios from "axios";
-import { fetchBreeds } from "./cat-api";
-
+import { fetchBreeds, fetchCatByBreed, BREEDS_VOID, loaderP, selectCats, catInfoDiv } from "./cat-api";
+// import Notiflix from 'notiflix';
 axios.defaults.headers.common["x-api-key"] = "live_cN2Up0hKvgIvqXtVFaUckvXElZKneik9YRcwu3Wa02xOF1zg0ESvEO80ojcDz33U";
-
-const selectCats = document.querySelector(".breed-select");
 const BASE_URL = `https://api.thecatapi.com/v1/breeds`;
-const BREEDS_VOID = `https://api.thecatapi.com/v1/images/`;
-const catInfoDiv = document.querySelector(".cat-info");
 
-console.log(fetchBreeds(BASE_URL)
+
+fetchBreeds(BASE_URL)
     .then((data) => selectCats.insertAdjacentHTML('beforeend', createMarkup(data)))
-    .catch((err) => console.log(err)));
+    .catch((err) => console.log(err))
 
 function createMarkup(arr) {
     return arr.map(({ reference_image_id, name }) => `
 <option value="${reference_image_id}">${name}</option>`).join();
 }
 
-selectCats.addEventListener("change", onSelectorChange);
-
-function onSelectorChange(e) {
-    const id = e.currentTarget.value;
-    console.log(id);
-    const currentUrl = `${BREEDS_VOID}${id}`;
-    fetchBreeds(currentUrl)
-        .then((data) =>
-catInfoDiv.insertAdjacentHTML("beforeend", `<img src="${data.url}" alt="${data.id}">
-<h1>${data.breeds[0].name}</h1>
+selectCats.addEventListener("change", (e) => fetchCatByBreed(e.currentTarget.value)
+    .then((data) => catInfoDiv.innerHTML =  `<img src="${data.url}"   alt="${data.id}" class="cat-image">
+<div class="cat-text">
+<h1 class="title">${data.breeds[0].name}</h1>
 <p>${data.breeds[0].description}</p>
-<p>${data.breeds[0].temperament}</p>`))     
-        .catch((err) => console.log(err));
-}
+<p><span class="title">Temperament:</span>${data.breeds[0].temperament}</p>
+</div>`)    
+    .catch((err) => console.log(err)));
 
 
